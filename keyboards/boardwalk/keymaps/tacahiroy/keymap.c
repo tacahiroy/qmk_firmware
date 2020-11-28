0e0a9386023d9291fb43ad91d5f3c76ebc4e91ed
Takahiro Yoshihara <tacahiroy@gmail.com>
2020-11-28 23:19:01 +0900

    Improve Colemak2

--- keyboards/boardwalk/keymaps/tacahiroy/keymap.c
+++ keyboards/boardwalk/keymaps/tacahiroy/keymap.c
@@ -52,10 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ),
 
  [_COLEMAK2] = LAYOUT_ortho_hhkb(
-  KC_GRV,   KC_1,  KC_2,     KC_3,     KC_4,   KC_5,  KC_MINS,  KC_EQL,   KC_6,  KC_7,   KC_8,     KC_9,        KC_0,     KC_BSPC, \
+  KC_GRV,   KC_1,  KC_2,     KC_3,     KC_4,   KC_5,  KC_PLUS,  KC_UNDS,  KC_6,  KC_7,   KC_8,     KC_9,        KC_0,     KC_BSPC, \
   KC_TAB,   KC_Q,  KC_W,     KC_F,     KC_P,   KC_G,  KC_HOME,  KC_END,   KC_J,  KC_L,   KC_U,     KC_Y,        KC_SCLN,  KC_QUOT, \
   KC_LCTL,  KC_A,  KC_R,     KC_S,     KC_T,   KC_D,  KC_PGUP,  KC_PGDN,  KC_H,  KC_N,   KC_E,     KC_I,        KC_O,     KC_ENT,  \
-  KC_LSFT,  KC_Z,  KC_X,     KC_C,     KC_V,   KC_B,  KC_DEL,   KC_BSPC,  KC_K,  KC_M,   KC_COMM,  KC_DOT,      KC_SLSH,  KC_RSFT, \
+  KC_LSFT,  KC_Z,  KC_X,     KC_C,     KC_V,   KC_B,  KC_EQL,   KC_MINS,  KC_K,  KC_M,   KC_COMM,  KC_DOT,      KC_SLSH,  KC_RSFT, \
             MOUS,  KC_LGUI,  KC_LALT,  LOWER,     KC_ESC,            KC_SPC,     RAISE,  KC_MINS,  MEH_X(BSLS), KC_RCTL            \
  ),
 
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LOWER] = LAYOUT_ortho_hhkb(
   MC_EXPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,  \
-  _______, _______, _______, _______, _______, _______, _______, _______, V_ALTB,  TM_PREV, TM_PWIN, TM_NEXT, _______, KC_PIPE, \
+  _______, _______, _______, _______, _______, _______, _______, _______, V_ALTB,  TM_PREV, W_IME,   TM_NEXT, _______, KC_PIPE, \
   _______, _______, _______, W_SSHOT, MOUS,    _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______, _______, \
-           _______, _______, _______, _______,    _______,           _______,      _______, W_IME,   _______, _______           \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_BSPC, _______, _______, \
+           _______, _______, _______, _______,    _______,           _______,      _______, _______, _______, _______           \
  ),
 
  [_RAISE] = LAYOUT_ortho_hhkb(
