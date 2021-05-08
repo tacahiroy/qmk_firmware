f8b38c014f2c29923d25e62c5798559bc4f14f6b
Takahiro Yoshihara <tacahiroy@gmail.com>
2021-05-08 23:13:41 +0900

    I type '6' with right pointing finger

--- keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
+++ keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
@@ -22,27 +22,27 @@ Copyright 2021 Takahiro Yoshihara <tacahiroy@gmail.com>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_alice(
-    KC_ESC,  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
-    KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_PGDN, KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_MEH,
-             MOUS,             KC_LALT, LOWER,   KC_ESC,                            KC_SPC,           RAISE,                              KC_RCTL
+    KC_ESC,  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,          KC_BSPC,
+    KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_PGDN, KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
+             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_LGUI, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_MEH,
+             MOUS,             KC_LALT, LOWER,   KC_ESC,                             RS_X(SPC),       RAISE,                              KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT_alice(
-    _______, _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           _______,
-    _______, _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, _______,
-    _______, _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          _______,
-             _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, _______,
-             _______,          _______, _______, _______,                           _______,          _______,                            _______
+    _______, _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS,          _______,
+    _______, _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, _______,
+    _______, _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN, KC_QUOT,          _______,
+             _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_LGUI, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, _______, _______,
+             _______,          _______, _______, _______,                            _______,         _______,                            _______
   ),
 
   [_LOWER] = LAYOUT_alice(
-    _______, MC_EXPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           _______,
+    _______, MC_EXPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F12,  KC_6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,            _______,
     _______, _______, _______, _______, _______, _______, TOP,              V_ALTB,  TM_PREV, KC_BSPC, TM_NEXT, _______, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, _______, _______, _______, W_SSHOT, MOUS,    W_IME,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______,          _______,
              _______, W_QUIT,  _______, _______, _______, BOTTOM,           _______, KC_HOME, KC_END,  _______, TM_LSTS, _______, _______, _______,
-             _______,          _______, _______, _______,                            _______,          _______,                            _______
+             _______,          _______, _______, _______,                            KC_SPC,           _______,                            _______
   ),
 
   [_RAISE] = LAYOUT_alice(
@@ -52,7 +52,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR,          _______, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______,
              _______,          _______, _______, _______,                            _______,          _______,                            _______
   ),
-
 
   [_MOUS] = LAYOUT_alice(
     RESET,   RGB_TOG, RGB_MOD, RGB_RMOD, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_MSTP,          KC_MPLY,
