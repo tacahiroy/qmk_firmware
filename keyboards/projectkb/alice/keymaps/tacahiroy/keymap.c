e0f89bd6db56bd808dd893937fdd42f54da3e030
Takahiro Yoshihara <tacahiroy@gmail.com>
2021-06-11 12:03:32 +0900

    MEH is not needed now

--- keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
+++ keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
@@ -19,13 +19,12 @@ Copyright 2021 Takahiro Yoshihara <tacahiroy@gmail.com>
 #include QMK_KEYBOARD_H
 #include "tacahiroy.h"
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_alice(
     KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,          KC_BSPC,
     KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_PGDN, KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,             KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_LGUI, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_MEH,
+             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_LGUI, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_DEL,
              MOUS,             KC_LALT, LOWER,   KC_ESC,                             RS_X(SPC),       RAISE,                              KC_RCTL
   ),
 
@@ -49,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, W_S_WIN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          W_QUIT,
     _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, FX_NEXT, _______, _______, _______, _______,
     _______, _______, _______, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC,          KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, _______, _______,          _______,
-             _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR,          _______, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______,
+             _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR,          KC_RCBR, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______,
              _______,          _______, _______, _______,                            _______,          _______,                            _______
   ),
 
