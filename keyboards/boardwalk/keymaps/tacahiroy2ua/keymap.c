e1483ddf8d92fe2dcea37dca0ab1a7544d73a755
Takahiro Yoshihara <tacahiroy@gmail.com>
2021-07-27 09:48:13 +0900

    is a better position for LOWER like other keebs

--- keyboards/boardwalk/keymaps/tacahiroy2ua/keymap.c
+++ keyboards/boardwalk/keymaps/tacahiroy2ua/keymap.c
@@ -19,11 +19,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_COLEMAK] = LAYOUT_2u_arrow(
-    KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,    KC_6,   KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_F,  KC_P,  KC_G,    KC_J,   KC_L, KC_U,    KC_Y,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_LCTL, KC_A,    KC_R,    KC_S,  KC_T,  KC_D,    KC_H,   KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT, KC_ENT,  KC_PGUP,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,    KC_K,   KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    MOUS,    KC_LGUI, KC_LALT, LOWER,        KC_ESC,  KC_SPC,       RAISE,   KC_MEH, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,       KC_6,   KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_F,  KC_P,  KC_G,       KC_J,   KC_L, KC_U,    KC_Y,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_LCTL, KC_A,    KC_R,    KC_S,  KC_T,  KC_D,       KC_H,   KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT, KC_ENT,  KC_PGUP,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,       KC_K,   KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    MOUS,    KC_LGUI, KC_LALT, LOWER,        LW_X(ESC),  KC_SPC,       RAISE,   KC_MEH, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
  [_QWERTY] = LAYOUT_2u_arrow(
