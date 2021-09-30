4750e37e4183f70b98a814410b1c49ffeff5251c
Takahiro Yoshihara <tacahiroy@gmail.com>
2021-09-30 14:32:08 +0900

    MEH -> ESC

--- keyboards/cannonkeys/atlas/keymaps/tacahiroy/keymap.c
+++ keyboards/cannonkeys/atlas/keymaps/tacahiroy/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q, KC_W,    KC_F,    KC_P,  KC_G,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_QUOT, \
   CTL_X(ESC), KC_A, KC_R,    KC_S,    KC_T,  KC_D,   KC_H,   KC_N,  KC_E,    KC_I,    KC_O,    KC_ENT,  \
   KC_LSFT,    KC_Z, KC_X,    KC_C,    KC_V,  KC_B,   KC_K,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_MPLY,    MOUS, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_BSLS, KC_RCTL, KC_MEH   \
+  KC_MPLY,    MOUS, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_BSLS, KC_RCTL, KC_ESC   \
 ),
 
 [_QWERTY] = LAYOUT_ortho_5x12( \
