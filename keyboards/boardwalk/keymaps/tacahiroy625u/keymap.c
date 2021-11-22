01f3c982817be16984df98261337f6d5f94e8502
Takahiro Yoshihara <tacahiroy@gmail.com>
2021-11-22 11:22:37 +0900

    LGUI is used more often

--- keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
+++ keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK] = LAYOUT_625u_arrow(
     KC_GRV,     KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6, KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,     KC_Q,    KC_W,    KC_F,  KC_P,  KC_G,  KC_J, KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    CTL_X(ESC), KC_A,    KC_R,    KC_S,  KC_T,  KC_D,  KC_H, KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, KC_ENT,  KC_LGUI,
+    CTL_X(ESC), KC_A,    KC_R,    KC_S,  KC_T,  KC_D,  KC_H, KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, KC_ENT,  MOUS,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,  KC_K, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    MOUS,       KC_LALT, LOWER,                   RS_X(SPC),                 RAISE,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LGUI,    KC_LALT, LOWER,                   RS_X(SPC),                 RAISE,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
  [_QWERTY] = LAYOUT_625u_arrow(
