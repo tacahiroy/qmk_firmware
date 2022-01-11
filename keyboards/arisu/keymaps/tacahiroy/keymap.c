9069fac810522bba626b271e050b69ad6bb5b0d2
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-01-11 14:27:56 +0900

    Try: Move BSPC to left thumb

--- keyboards/arisu/keymaps/tacahiroy/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,  KC_W,  KC_F,   KC_P,     KC_G,            KC_J,       KC_L,   KC_U,     KC_Y,    KC_SCLN,      KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
     KC_LCTL,  KC_A,  KC_R,  KC_S,   KC_T,     KC_D,            KC_H,       KC_N,   KC_E,     KC_I,    KC_O,         KC_QUOT,  KC_ENT,             KC_PGDN,
     KC_LSFT,  KC_Z,  KC_X,  KC_C,   KC_V,     KC_B,            KC_K,       KC_M,   KC_COMM,  KC_DOT,  CTL_X(SLSH),  KC_RSFT,  KC_UP,
-    KC_LGUI,  KC_LALT,      LOWER,  KC_ESC,                    RS_X(SPC),  RAISE,                                   KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LGUI,  KC_LALT,      LOWER,  KC_BSPC,                   RS_X(SPC),  RAISE,                                   KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_QWERTY] = LAYOUT(
