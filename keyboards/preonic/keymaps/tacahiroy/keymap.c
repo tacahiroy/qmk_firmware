f22688118b41b030469716d6bb8e958edcc6d772
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-02-21 12:01:22 +0900

    BS and QUOT

--- keyboards/preonic/keymaps/tacahiroy/keymap.c
+++ keyboards/preonic/keymaps/tacahiroy/keymap.c
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT_preonic_grid(
   KC_GRV,     KC_1, KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,
-  KC_TAB,     KC_Q, KC_W,    KC_F,    KC_P,  KC_G,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+  KC_TAB,     KC_Q, KC_W,    KC_F,    KC_P,  KC_G,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
   CTL_X(ESC), KC_A, KC_R,    KC_S,    KC_T,  KC_D,   KC_H,   KC_N,  KC_E,    KC_I,    KC_O,    KC_ENT,
   KC_LSFT,    KC_Z, KC_X,    KC_C,    KC_V,  KC_B,   KC_K,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_MPLY,    MOUS, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_BSLS, KC_RCTL, KC_QUOT
+  KC_MPLY,    MOUS, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_BSLS, KC_RCTL, KC_ESC
 ),
 
 [_QWERTY] = LAYOUT_preonic_grid(
