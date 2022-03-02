bbfeffa19f9c576f04cdb3f35494b74363e24d12
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-03-02 14:28:00 +0900

    VOLU / VOLD

--- keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
+++ keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
@@ -40,12 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY,
     M_EXPS,  XX_WIN,  XX_TAB,  KC_MS_U, W_SSHOT, _______, _______, TM_PREV, KC_BSPC, TM_NEXT, KC_PIPE, KC_BTN1, KC_BTN2, KC_PIPE,
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, MOUS,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______, RESET,
-    _______, _______, _______, _______, _______, TOP,     BOTTOM,  KC_HOME, KC_END,  KC_SPC,  TM_LSTS, _______, KC_PGUP, KC_PGDN,
-    _______, _______, _______,                      KC_HENK,                         _______, _______, _______, KC_VOLU, KC_VOLD
+    _______, _______, _______, _______, _______, TOP,     BOTTOM,  KC_HOME, KC_END,  KC_SPC,  TM_LSTS, _______, KC_VOLU, _______,
+    _______, _______, _______,                      KC_HENK,                         _______, _______, KC_PGUP, KC_VOLD, KC_PGDN
+
   ),
 
  [_RAISE] = LAYOUT_625u_arrow(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, W_QUIT,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XX_WIN,
     W_S_WIN, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, FX_NEXT, _______, _______, _______, RAL_RCT,
     _______, KC_MINS, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, _______, _______, _______, _______,
     _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______, _______,
