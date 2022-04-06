1b52a9ced6d11f4b3242f11cf750aa4cf5c19c0d
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-04-06 17:54:58 +0900

    PGUP / DN, VOLU / VOLD

--- keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
+++ keyboards/boardwalk/keymaps/tacahiroy625u/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK] = LAYOUT_625u_arrow(
     KC_GRV,      KC_1,     KC_2,    KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
     KC_TAB,      KC_Q,     KC_W,    KC_F,  KC_P,  KC_G,  KC_J,  KC_L,  KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
-    CTL_X(ESC),  KC_A,     KC_R,    KC_S,  KC_T,  KC_D,  KC_H,  KC_N,  KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,   KC_ESC,
+    CTL_X(ESC),  KC_A,     KC_R,    KC_S,  KC_T,  KC_D,  KC_H,  KC_N,  KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,   KC_PGUP,
     KC_LSFT,     KC_Z,     KC_X,    KC_C,  KC_V,  KC_B,  KC_K,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,
     KC_LGUI,     KC_LALT,  LOWER,                   RS_X(SPC),                   RAISE,   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LOWER] = LAYOUT_625u_arrow(
     W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY,
-    M_EXPS,  XX_WIN,  XX_TAB,  KC_MS_U, W_SSHOT, _______, _______, TM_PREV, KC_BSPC, TM_NEXT, KC_PIPE, KC_BTN1, KC_BTN2, KC_PIPE,
+    M_EXPS,  XX_WIN,  XX_TAB,  KC_MS_U, W_SSHOT, KC_BTN1, KC_BTN2, TM_PREV, KC_BSPC, TM_NEXT, KC_PIPE, _______, KC_VOLD, KC_VOLU,
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, MOUS,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______, RESET,
-    _______, _______, _______, _______, _______, TOP,     BOTTOM,  KC_HOME, KC_END,  KC_SPC,  TM_LSTS, _______, KC_VOLU, _______,
-    _______, _______, _______,                      KC_HENK,                         _______, _______, KC_PGUP, KC_VOLD, KC_PGDN
+    _______, _______, _______, _______, _______, TOP,     BOTTOM,  KC_HOME, KC_END,  KC_SPC,  TM_LSTS, _______, _______, _______,
+    _______, _______, _______,                      KC_HENK,                         _______, _______, _______, _______, _______
 
   ),
 
  [_RAISE] = LAYOUT_625u_arrow(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XX_WIN,
+    _______, _______, _______, _______, _______, _______, W_LFT_D, W_RGT_D, _______, _______, _______, _______, _______, XX_WIN,
     W_S_WIN, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, FX_NEXT, _______, _______, _______, RAL_RCT,
     _______, KC_MINS, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, _______, _______, _______, _______,
     _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______, _______,
