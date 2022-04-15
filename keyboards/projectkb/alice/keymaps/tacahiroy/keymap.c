66d417e6c6accd766c14808bc532595f397d0707
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-04-15 16:01:27 +0900

    May be bit useful

--- keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
+++ keyboards/projectkb/alice/keymaps/tacahiroy/keymap.c
@@ -24,8 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_GRV,     KC_1, KC_2, KC_3,    KC_4,  KC_5,   KC_EQL,  KC_6,      KC_7,  KC_8, KC_9,    KC_0,    KC_MINS,          KC_BSPC,
     KC_PGUP, KC_TAB,     KC_Q, KC_W, KC_F,    KC_P,  KC_G,            KC_J,      KC_L,  KC_U, KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_PGDN, CTL_X(ESC), KC_A, KC_R, KC_S,    KC_T,  KC_D,            KC_H,      KC_N,  KC_E, KC_I,    KC_O,    KC_QUOT,          KC_ENT,
-             KC_LSFT,    KC_Z, KC_X, KC_C,    KC_V,  KC_B,            KC_K,      KC_K,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_MPLY,
-
+             KC_LSFT,    KC_Z, KC_X, KC_C,    KC_V,  KC_B,            KC_K,      KC_K,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_PGDN,
              KC_LGUI,                KC_LALT, LOWER, KC_ESC,          RS_X(SPC), RAISE,                                       KC_RCTL
   ),
 
@@ -41,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MPLY, W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F12,   KC_6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,           _______,
     KC_VOLU, M_EXPS,  XX_WIN,  XX_TAB,  KC_MS_U, W_SSHOT, KC_BTN1,           KC_BTN2, TM_PREV, KC_BSPC, TM_NEXT, KC_PIPE, KC_LCBR, KC_RCBR, _______,
     KC_VOLD, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______,          _______,
-             _______, _______, _______, _______, _______, TOP,               BOTTOM,  KC_HOME, KC_END,  _______, _______, TM_LSTS, _______, _______,
+             _______, _______, _______, _______, _______, TOP,               BOTTOM,  KC_HOME, KC_END,  _______, _______, TM_LSTS, _______, KC_PGUP,
              _______,                   _______, _______, _______,           M_HENK,  _______,                                         _______
   ),
 
