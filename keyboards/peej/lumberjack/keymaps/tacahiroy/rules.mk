6677930b8350ef8707a4fe4b6044aa39767fec1e
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-06-08 16:40:33 +0900

    Add Lumberjack R2

---
+++ keyboards/peej/lumberjack/keymaps/tacahiroy/rules.mk
@@ -0,0 +1,17 @@
+BOOTMAGIC_ENABLE = yes
+COMMAND_ENABLE   = no
+DEBUG_ENABLE     = no
+CONSOLE_ENABLE   = no
+TAP_DANCE_ENABLE = no
+NKRO_ENABLE      = yes
+LEADER_ENABLE    = no
+
+AUDIO_ENABLE = no
+
+SLEEP_LED_ENABLE     = no
+LCD_BACKLIGHT_ENABLE = no
+LCD_ENABLE           = no
+
+RGBLIGHT_ENABLE      = no  # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+
+MOUSEKEY_ENABLE      = yes
