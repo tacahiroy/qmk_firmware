fef3fe8169483c9a9af368ec2567650ca3729ccf
Takahiro Yoshihara <tacahiroy@gmail.com>
2022-12-02 10:53:01 +0900

    F2 and F4 for Excel!

--- keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
+++ keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_RAISE] = LAYOUT_default(
         W_S_WIN,  KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  FX_NEXT,  _______,  _______,  _______,  XX_WIN,
-        KC_VOLU,  _______,  _______,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,  KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  RAL_RCT,
-        KC_VOLD,  _______,  XXXXXXX,  _______,  CUT,      COPY2,    PASTE2,   KC_LCBR,  KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,  _______,
+        KC_VOLU,  _______,  KC_F2,    KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,  KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  RAL_RCT,
+        KC_VOLD,  _______,  XXXXXXX,  KC_F4,    CUT,      COPY2,    PASTE2,   KC_LCBR,  KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,  _______,
         _______,            _______,  _______,            _______,            _______,            _______,            _______
     ),
 
