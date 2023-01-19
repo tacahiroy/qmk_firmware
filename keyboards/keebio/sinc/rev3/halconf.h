88cec2b163e9644fb959d32940e11949011197ef
Nick Brassel <nick@tzarc.org>
2023-01-19 12:05:29 +1100

    Fixup ChibiOS header inclusion search ordering. (#19623)
    * Add STM32F446-Nucleo onekey.
    
    * Fixup onekey build for F446, all keymaps.
    
    * Fixup board inclusion search ordering.

--- keyboards/keebio/sinc/rev3/halconf.h
+++ keyboards/keebio/sinc/rev3/halconf.h
@@ -1,10 +1,4 @@
-<<<<<<<< HEAD:keyboards/handwired/riblee_f401/keymaps/riblee/config.h
-/* Copyright 2020 Daniel Reibl <janos.daniel.reibl@protonmail.com>
-|||||||| d13f5f0d66:keyboards/horrortroll/chinese_pcb/black_e65/keymaps/via/config.h
-/* Copyright 2021 HorrorTroll <https://github.com/HorrorTroll>
-========
 /* Copyright 2022 QMK
->>>>>>>> upstream/master:keyboards/keebio/sinc/rev3/halconf.h
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,14 +15,7 @@
  */
 
 #pragma once
-<<<<<<<< HEAD:keyboards/handwired/riblee_f401/keymaps/riblee/config.h
-#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX
-|||||||| d13f5f0d66:keyboards/horrortroll/chinese_pcb/black_e65/keymaps/via/config.h
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 2
-========
 
 #define HAL_USE_I2C TRUE
 
-#include_next "halconf.h"
->>>>>>>> upstream/master:keyboards/keebio/sinc/rev3/halconf.h
+#include_next <halconf.h>
