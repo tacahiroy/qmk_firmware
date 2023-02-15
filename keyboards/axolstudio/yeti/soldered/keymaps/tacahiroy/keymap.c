a04a3090f8fb926b7a9ea37de84b187c9b381993
Takahiro Yoshihara <tacahiroy@gmail.com>
2023-02-15 14:25:41 +0900

    HOME/END and PGUP/PGDN to be the same place as other boards

--- keyboards/axolstudio/yeti/soldered/keymaps/tacahiroy/keymap.c
+++ keyboards/axolstudio/yeti/soldered/keymaps/tacahiroy/keymap.c
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MPLY,   W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F12,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, KC_DEL,
     KC_VOLU,   M_EXPS,  XX_WIN,  XX_TAB,  _______, _______, _______,           _______, TM_PREV, KC_BSPC, TM_NEXT, KC_PIPE, KC_LCBR, KC_RCBR, _______,
     KC_VOLD,   _______, KC_ESC,  MOUS,    W_SSHOT, KC_F5,   KC_F3,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______,
-               _______, _______, _______, _______, _______, TOP,               BOTTOM,  KC_HOME, KC_END,  _______, _______, TM_LSTS, _______, _______,
+               _______, _______, _______, _______, _______, TOP,               BOTTOM,  _______, KC_HOME, KC_END,  _______, TM_LSTS, _______, _______,
                _______,                   _______, _______, _______,           JP_HENK, _______,                                              _______
   ),
 
   [_RAISE] = LAYOUT_alice_split_bs(
     QK_BOOT,   _______, _______, _______, KC_F3,   _______, KC_F5,   _______,   WS_LEFT, WS_RGHT, _______, _______, _______, _______, _______, XX_WIN,
     _______,   W_S_WIN, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,            KC_CIRC, KC_AMPR, KC_ASTR, FX_NEXT, _______, _______, _______, _______,
-    _______,   _______, _______, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC,            KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, MIC_TGL, _______, _______,
-               _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR,            KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______, _______,
+    KC_MUTE,   _______, _______, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC,            KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, MIC_TGL, _______, _______,
+               _______, KC_UNDS, CUT,     COPY2,   PASTE2,  KC_LCBR,            KC_RCBR, _______, KC_PGUP, KC_PGDN, _______, _______, _______, _______,
                _______,                   _______, _______, _______,            _______, _______,                                              _______
   ),
 
