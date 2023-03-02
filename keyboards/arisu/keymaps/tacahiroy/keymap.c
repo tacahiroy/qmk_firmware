086cccd454d75c2793db2c5fd8f3ab8f0f6a503e
Takahiro Yoshihara <tacahiroy@gmail.com>
2023-03-02 14:10:39 +0900

    Swap BSLS and BSPC

--- keyboards/arisu/keymaps/tacahiroy/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy/keymap.c
@@ -20,11 +20,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,  KC_DEL,
-    KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSLS,  KC_PGUP,
+    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSLS,  KC_DEL,
+    KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSPC,  KC_PGUP,
     CTL_X(ESC),  KC_A,  KC_R,     KC_S,  KC_T,   KC_D,             KC_H,       KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT,     KC_Z,  KC_X,     KC_C,  KC_V,   KC_B,             KC_K,       KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,
-    KC_LGUI,            KC_LALT,         LOWER,  RAISE,            RS_X(SPC),  KC_RCTL,                               KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LGUI,            KC_LALT,         LOWER,  RAISE,            RS_X(SPC),  KC_RCTL,                               KC_LEFT,  KC_DOWN, KC_RGHT
   ),
 
   [_QWERTY] = LAYOUT(
@@ -36,17 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    W_IME,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  KC_DEL,   KC_MPLY,
-    M_EXPS,   XX_WIN,   XX_TAB,   _______,  _______,  _______,           _______,  TM_PREV,  KC_BSPC,  TM_NEXT,  KC_PIPE,  _______,  _______,  _______,  KC_VOLU,
+    W_IME,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  _______,  KC_MPLY,
+    M_EXPS,   XX_WIN,   XX_TAB,   _______,  _______,  _______,           _______,  TM_PREV,  KC_BSPC,  TM_NEXT,  KC_PIPE,  _______,  _______,  KC_DEL,   KC_VOLU,
     _______,  KC_ESC,   MOUS,     W_SSHOT,  KC_F5,    KC_F3,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   _______,  _______,            KC_VOLD,
     _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  TM_LSTS,  _______,  _______,
     _______,            _______,            _______,  _______,           JP_HENK,  KC_ESC,                                 _______,  _______,  _______
   ),
 
   [_RAISE] = LAYOUT(
-    _______,  _______,  _______,  KC_F3,    _______,  KC_F5,    _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  XX_WIN,  _______,
+    _______,  _______,  _______,  KC_F3,    _______,  KC_F5,    _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  XX_WIN,  QK_BOOT,
     W_S_WIN,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,           KC_CIRC,  KC_AMPR,  KC_ASTR,  FX_NEXT,  _______,  _______,  _______,  RAL_RCT, _______,
-    _______,  _______,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,           KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  _______,           QK_BOOT,
+    _______,  _______,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,           KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  _______,           _______,
     _______,  KC_UNDS,  CUT,      COPY2,    PASTE2,   KC_LCBR,           KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,  _______,
     _______,            _______,            _______,  _______,           _______,  _______,                                _______,  _______,  _______
   ),
--- keyboards/arisu/keymaps/tacahiroy2u/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy2u/keymap.c
@@ -20,8 +20,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSPC, KC_BSPC,  KC_ESC,
-    KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSLS,  KC_PGUP,
+    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSPC, KC_BSLS,  KC_ESC,
+    KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSPC,  KC_PGUP,
     CTL_X(ESC),  KC_A,  KC_R,     KC_S,  KC_T,   KC_D,             KC_H,       KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT,     KC_Z,  KC_X,     KC_C,  KC_V,   KC_B,             KC_K,       KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,
     KC_LGUI,            KC_LALT,         LOWER,  RAISE,            RS_X(SPC),  KC_RCTL,                               KC_LEFT,  KC_DOWN,  KC_RGHT
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT(
-    _______,  _______,  _______,  KC_F3,    _______,  KC_F5,    _______, _______,  _______,  _______,  _______,  _______,  _______,  XX_WIN,   XX_WIN,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,  _______,  XX_WIN,   XX_WIN,  QK_BOOT,
     W_S_WIN,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,           KC_CIRC,  KC_AMPR,  KC_ASTR,  FX_NEXT,  _______,  _______,  _______,  RAL_RCT, _______,
-    _______,  _______,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,           KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  _______,           QK_BOOT,
+    _______,  _______,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,           KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  MIC_TGL,  _______,  _______,           _______,
     _______,  KC_UNDS,  CUT,      COPY2,    PASTE2,   KC_LCBR,           KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,  _______,
     _______,            _______,            _______,  _______,           _______,  _______,                                _______,  _______,  _______
   ),
