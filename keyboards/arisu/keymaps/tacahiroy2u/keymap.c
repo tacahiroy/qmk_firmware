2ca3ea6676e5c3509ff321c95f00e4cb46cd7a88
Takahiro Yoshihara <tacahiroy@gmail.com>
2023-03-02 14:14:00 +0900

    yes, it's 2u BS

--- keyboards/arisu/keymaps/tacahiroy2u/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy2u/keymap.c
@@ -20,7 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSPC, KC_BSLS,  KC_ESC,
+    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSLS, KC_BSLS,  KC_ESC,
     KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSPC,  KC_PGUP,
     CTL_X(ESC),  KC_A,  KC_R,     KC_S,  KC_T,   KC_D,             KC_H,       KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT,     KC_Z,  KC_X,     KC_C,  KC_V,   KC_B,             KC_K,       KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    W_IME,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_DEL,   KC_DEL,   KC_MPLY,
-    M_EXPS,   XX_WIN,   XX_TAB,   _______,  _______,  _______,           _______,  TM_PREV,  KC_BSPC,  TM_NEXT,  KC_PIPE,  _______,  _______,  _______,  KC_VOLU,
+    W_IME,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   _______,  _______,  KC_MPLY,
+    M_EXPS,   XX_WIN,   XX_TAB,   _______,  _______,  _______,           _______,  TM_PREV,  KC_BSPC,  TM_NEXT,  KC_PIPE,  _______,  _______,  KC_DEL,   KC_VOLU,
     _______,  KC_ESC,   MOUS,     W_SSHOT,  KC_F5,    KC_F3,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   _______,  _______,            KC_VOLD,
     _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  TM_LSTS,  _______,  _______,
     _______,            _______,            _______,  _______,           JP_HENK,  KC_ESC,                                 _______,  _______,  _______
