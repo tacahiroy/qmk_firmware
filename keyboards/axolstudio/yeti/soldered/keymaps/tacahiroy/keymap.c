b4471a2cbeb289184e3d7d68452a58ba2b86c365
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-01-18 11:50:44 +0900

    Adjust RESET and Tmux last-session

--- keyboards/axolstudio/yeti/soldered/keymaps/tacahiroy/keymap.c
+++ keyboards/axolstudio/yeti/soldered/keymaps/tacahiroy/keymap.c
@@ -14,14 +14,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Copyright 2022-2023 Takahiro Yoshihara <tacahiroy@gmail.com>
+Copyright 2022-2024 Takahiro YOSHIHARA <tacahiroy@gmail.com>
 */
 #include QMK_KEYBOARD_H
 #include "tacahiroy.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_alice_split_bs(
-    KC_MPLY,   QK_GESC,    KC_1,   KC_2,   KC_3,      KC_4,    KC_5,   KC_EQL,  KC_6,      KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,
+    KC_MPLY,   QK_GESC,    KC_1,   KC_2,   KC_3,      KC_4,    KC_5,   KC_GRV,  KC_6,      KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,
     KC_PGUP,   KC_TAB,     KC_Q,   KC_W,   KC_F,      KC_P,    KC_G,            KC_J,      KC_L,    KC_U,   KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
     KC_PGDN,   CTL_X(ESC), KC_A,   KC_R,   KC_S,      KC_T,    KC_D,            KC_H,      KC_N,    KC_E,   KC_I,    KC_O,    KC_QUOT, KC_ENT,
                SC_LSPO,    KC_Z,   KC_X,   KC_C,      KC_V,    KC_B,            KC_K,      KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, KC_PGDN,
@@ -39,16 +39,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_alice_split_bs(
     KC_MPLY,   W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
     KC_VOLU,   M_EXPS,  XX_WIN,  XX_TAB,  KC_GRV,  _______, _______,           TM_SLST, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, KC_MPRV, KC_MNXT, KC_DEL,
-    KC_VOLD,   _______, KC_ESC,  MOUS,    W_SNIP, KC_F5,   KC_F3,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______,
-               _______, _______, _______, _______, _______, TOP,               _______, BOTTOM,  KC_HOME, KC_END,  _______, TM_LASS, _______, KC_PGUP,
+    KC_VOLD,   _______, _______, MOUS,    W_SNIP,  KC_F5,   KC_F3,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  TM_LASS, _______,
+               _______, _______, _______, _______, _______, TOP,               _______, BOTTOM,  KC_HOME, KC_END,  _______, _______, _______, KC_PGUP,
                _______,                   _______, _______, _______,           JP_HENK, _______,                                              _______
   ),
 
   [_RAISE] = LAYOUT_alice_split_bs(
-    QK_BOOT,   _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, XX_WIN,
+    _______,   _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, XX_WIN,
     _______,   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,            KC_CIRC, KC_AMPR, KC_ASTR, FX_NEXT, _______, _______, _______, _______,
     KC_MUTE,   CW_TOGG, KC_UNDS, KC_EQL,  KC_PLUS, KC_LPRN, KC_LBRC,            KC_RBRC, KC_RPRN, KC_DEL,  KC_BSLS, _______, _______, _______,
-               _______, _______, CUT,     COPY2,   PASTE2,  KC_LCBR,            _______, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, _______,
+               _______, _______, CUT,     COPY2,   PASTE2,  KC_LCBR,            _______, KC_RCBR, KC_PGUP, KC_PGDN, _______, _______, _______, QK_BOOT,
                _______,                   _______, _______, _______,            _______, _______,                                              _______
   ),
 
--- keyboards/chlx/merro60/keymaps/tacahiroy/keymap.c
+++ keyboards/chlx/merro60/keymaps/tacahiroy/keymap.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 Takahiro Yoshihara
+ * Copyright 2023-2024 Takahiro YOSHIHARA <tacahiroy@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
