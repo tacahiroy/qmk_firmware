50afc8ab5955b4d841b4a2e9545449bc2e062e8e
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-04-30 11:02:21 +0900

    Remove a shortcut for Parsec

--- keyboards/hineybush/h60/keymaps/tacahiroy/keymap.c
+++ keyboards/hineybush/h60/keymaps/tacahiroy/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,     KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
     CTL_X(ESC), KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,     KC_I,    KC_O,    KC_QUOT, KC_ENT,
     SC_LSPO,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, SC_RSPC, KC_PGDN,
-    KC_LGUI,    KC_LALT, LOWER,                          RS_X(SPC),                KC_RCTL, KC_RALT, PS_IMER
+    KC_LGUI,    KC_LALT, LOWER,                          RS_X(SPC),                KC_RCTL, KC_RALT, KC_RGUI
   ),
 
   [_HHKB] = LAYOUT_60_tsangan_hhkb(
