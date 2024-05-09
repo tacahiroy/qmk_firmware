745e68c302cc6a48c7e5c5eaa101d928119f6e21
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-05-09 18:34:06 +0900

    F13 and CAPS

--- keyboards/kkatano/bakeneko60/keymaps/tacahiroy/keymap.c
+++ keyboards/kkatano/bakeneko60/keymaps/tacahiroy/keymap.c
@@ -23,8 +23,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,     KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_ESC,
     KC_TAB,     KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,     KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,
     CTL_X(ESC), KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,     KC_I,    KC_O,    KC_QUOT, KC_ENT,
-    SC_LSPO,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, SC_RSPC, KC_PGUP,
-    KC_LGUI,    KC_LALT, LOWER,                          RS_X(SPC),                KC_RCTL, KC_RALT, KC_PGDN
+    SC_LSPO,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, SC_RSPC, KC_PGDN,
+    KC_LGUI,    KC_LALT, LOWER,                          RS_X(SPC),                KC_RCTL, KC_RALT, KC_RGUI
   ),
 
   [_HHKB] = LAYOUT_60_tsangan_hhkb(
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_60_tsangan_hhkb(
-    W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+    W_IME,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13, _______,
     W_WLST,  _______, XX_TAB,  PS_DETM, PS_WINM, PS_IMER, TM_SLST, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, KC_VOLD, KC_VOLU, KC_MPLY,
     _______, _______, MOUS,    W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  TM_LASS, _______,
-    _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______, _______,
-    _______, _______, _______,                   JP_HENK,                            _______, _______, _______ 
+    _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______, KC_PGUP,
+    _______, _______, _______,                   JP_HENK,                            _______, _______, KC_CAPS 
   ),
 
 
