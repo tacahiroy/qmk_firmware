1bf0eeed29c63e443a9d215eb81415243c017643
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-05-13 15:43:14 +0900

    Add CAPS like other boards

--- keyboards/hineybush/h60/keymaps/tacahiroy/keymap.c
+++ keyboards/hineybush/h60/keymaps/tacahiroy/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     W_WLST,  _______, XX_TAB,  PS_DETM, PS_WINM, PS_IMER, TM_SLST, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, KC_VOLD, KC_VOLU, KC_MPLY,
     _______, _______, MOUS,    W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  TM_LASS, _______,
     _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______, KC_PGUP,
-    _______, _______, _______,                   JP_HENK,                            _______, _______, _______ 
+    _______, _______, _______,                   JP_HENK,                            _______, _______, KC_CAPS 
   ),
 
 
