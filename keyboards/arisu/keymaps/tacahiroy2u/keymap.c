b5acba9b7d427db74f75e8ae103048ea8718339e
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-06-27 17:53:31 +0900

    Add CAPS

--- keyboards/arisu/keymaps/tacahiroy2u/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy2u/keymap.c
@@ -20,7 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    QK_GESC,     KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSPC, _______,  KC_GRV,
+    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_BSPC, _______,  KC_ESC,
     KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSLS,  KC_PGUP,
     CTL_X(ESC),  KC_A,  KC_R,     KC_S,  KC_T,   KC_D,             KC_H,       KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,            KC_PGDN,
     SC_LSPO,     KC_Z,  KC_X,     KC_C,  KC_V,   KC_B,             KC_K,       KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,  KC_UP,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_DEL,   KC_DEL,  KC_MUTE,
     W_WLST,   _______,  XX_TAB,   PS_DETM,  PS_WINM,  PS_IMER,           TM_SLST,  TM_PREV,  TM_WLST,  TM_NEXT,  KC_PIPE,  KC_VOLD,  KC_VOLU,  KC_MPLY, KC_MPRV,
     _______,  _______,  MOUS,     W_SNIP,   KC_F5,    KC_F3,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   TM_LASS,  _______,           KC_MNXT,
-    _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  _______,  TOP,
+    _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  KC_CAPS,  TOP,
     _______,            _______,            _______,  _______,           JP_HENK,  _______,                                KC_HOME,  BOTTOM,  KC_END
   ),
 
