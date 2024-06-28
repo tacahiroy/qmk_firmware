47ef8be6645e1a8a0ffc1a20ab955d840f246c2b
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-06-28 09:54:31 +0900

    Top-left should be better to be ESC

--- keyboards/arisu/keymaps/tacahiroy/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy/keymap.c
@@ -20,7 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_GRV,      KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSLS,  KC_ESC,
+    QK_GESC,     KC_1,  KC_2,     KC_3,  KC_4,   KC_5,    KC_EQL,  KC_6,       KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSLS,  KC_GRV,
     KC_TAB,      KC_Q,  KC_W,     KC_F,  KC_P,   KC_G,             KC_J,       KC_L,     KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSPC,  KC_PGUP,
     CTL_X(ESC),  KC_A,  KC_R,     KC_S,  KC_T,   KC_D,             KC_H,       KC_N,     KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,            KC_PGDN,
     SC_LSPO,     KC_Z,  KC_X,     KC_C,  KC_V,   KC_B,             KC_K,       KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,           KC_UP,
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    W_IME,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_MUTE,
+    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______, KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_MUTE,
     W_WLST,   _______,  XX_TAB,   PS_DETM,  PS_WINM,  PS_IMER,           TM_SLST,  TM_PREV,  TM_WLST,  TM_NEXT,  KC_PIPE,  KC_VOLD,  KC_VOLU,  KC_MPLY,  KC_MPRV,
     _______,  _______,  MOUS,     W_SNIP,   KC_F5,    KC_F3,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   TM_LASS,  _______,            KC_MNXT,
     _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  KC_CAPS,            _______,
