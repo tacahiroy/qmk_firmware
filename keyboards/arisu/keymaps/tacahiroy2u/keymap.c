7100db668cb52a21dd9085721c71c77907c0e739
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-10-16 17:51:23 +0900

    Arisu: Move F-row to RAISE to be the same as others

--- keyboards/arisu/keymaps/tacahiroy2u/keymap.c
+++ keyboards/arisu/keymaps/tacahiroy2u/keymap.c
@@ -36,15 +36,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_DEL,   KC_DEL,  KC_MUTE,
+    KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______, _______,  C_LEFT,   C_RIGHT,  _______,  _______,  _______,  KC_CAPS,  KC_CAPS, _______,
     W_WLST,   _______,  XX_TAB,   PS_DETM,  PS_WINM,  PS_IMER,           TM_SLST,  TM_PREV,  TM_WLST,  TM_NEXT,  KC_PIPE,  KC_VOLD,  KC_VOLU,  KC_MPLY, KC_MPRV,
     _______,  _______,  MOUS,     W_SNIP,   KC_F5,    KC_F3,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   TM_LASS,  _______,           KC_MNXT,
-    _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  KC_CAPS,  TOP,
+    _______,  _______,  _______,  _______,  _______,  TOP,               BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  _______,  TOP,
     _______,            _______,            _______,  _______,           JP_HENK,  _______,                                KC_HOME,  BOTTOM,  KC_END
   ),
 
   [_RAISE] = LAYOUT(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,  _______,  XX_WIN,   XX_WIN,  QK_BOOT,
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F12,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   XX_WIN,   XX_WIN,  QK_BOOT,
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,           KC_CIRC,  KC_AMPR,  KC_ASTR,  FX_NEXT,  _______,  _______,  _______,  _______, _______,
     CW_TOGG,  KC_UNDS,  KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,           KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  _______,  _______,  _______,           _______,
     _______,  _______,  CUT,      COPY2,    PASTE2,   KC_LCBR,           KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,  _______,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_MOUS] = LAYOUT(
-    QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,
     _______,  _______,  _______,  _______,  _______,  _______,           _______,  KC_BTN1,  _______,  KC_BTN2,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,           KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  _______,  _______,  _______,            _______,
     _______,  _______,  _______,  _______,  _______,  _______,           _______,  _______,  _______,  _______,  _______,  _______,  _______,
