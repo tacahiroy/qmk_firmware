b564fb0ee5e438cb7cd5a22e7c4e53aed8f53fff
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-12-03 09:40:18 +0900

    Putting Ctrl and ESC to one key leads some problems with my SKK key bindings

--- keyboards/pizzakeyboards/pizza65/keymaps/tacahiroy/keymap.c
+++ keyboards/pizzakeyboards/pizza65/keymaps/tacahiroy/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_65_ansi_blocker(
         QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_DEL,
         KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,   KC_PGUP,
-        CTL_X(ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,    KC_PGDN,
+        KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,    KC_PGDN,
         SC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,          KC_UP,     MOUS,
         KC_LGUI,    KC_LALT, LOWER,                     RS_X(SPC),                         KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN,   KC_RGHT
     ),
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         W_WLST,  XX_WIN,  XX_TAB,  PS_DETM, PS_WINM, PS_IMER, TM_SLST, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, _______, _______, _______,   KC_VOLU,
         KC_CAPS, KC_MPLY, MOUS,    W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  TM_LASS, _______,            KC_VOLD,
         _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______,          TOP,       _______,
-        _______, _______, _______,                   JP_HENK,                            _______, _______,          C_LEFT,  BOTTOM,    C_RIGHT
+        _______, _______, _______,                   JP_HENK,                            _______, _______,          _______, BOTTOM,    _______
     ),
 
     [_RAISE] = LAYOUT_65_ansi_blocker(
