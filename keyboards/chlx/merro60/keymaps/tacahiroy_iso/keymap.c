2daaccbdee5cb83d7898e6858c1e9fb33162859e
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-01-14 14:51:13 +0900

    Optimise ADJUST layer

--- keyboards/chlx/merro60/keymaps/tacahiroy_iso/keymap.c
+++ keyboards/chlx/merro60/keymaps/tacahiroy_iso/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_iso_tsangan(
     QK_GESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_BSPC,
     KC_TAB,   KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,   KC_Y,     KC_SCLN, KC_LBRC, KC_RBRC,
-    KC_LCTL,  KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,     KC_O,    KC_QUOT, KC_GRV, KC_ENT,
+    KC_LCTL,  KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,     KC_O,    KC_QUOT, KC_GRV,  KC_ENT,
     SC_LSPO,  KC_NUBS, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, SC_RSPC, KC_PGDN,
     KC_LGUI,  KC_LALT, LOWER,                                          RS_X(SPC),                 KC_RCTL, KC_RALT, KC_RGUI
   ),
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT_iso_tsangan(
     QK_BOOT, _______, EPRM,    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SYRQ, KC_SCRL, KC_BRK,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, QWERTY,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, CADEL,   _______, AC_TOGG, _______, DB_TOGG, HHKB,    NORMAN,  _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, VERSION, _______, _______, QWERTY,  COLEMAK, _______, _______, _______, _______,
+    _______, _______, _______, _______, COLEMAK, VERSION, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______,                            _______,                   _______, _______, _______ 
   )
 };
