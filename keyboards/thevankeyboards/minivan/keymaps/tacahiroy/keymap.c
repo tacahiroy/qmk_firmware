f7d8eaec0e926a0394b8ee62ad31417a206f9cf7
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-07-03 19:04:41 +0900

    Left spacebar is easy to hold in MiniVan

--- keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
     CTL_X(ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LGUI,    KC_LALT, LOWER,                 MV_X(ENT),   RS_X(SPC),        KC_BSPC, RAISE,   KC_RCTL, KC_RALT
+    KC_LGUI,    KC_LALT, MOVE,                 LW_X(ENT),   RS_X(SPC),        KC_BSPC, RAISE,   KC_RCTL, KC_RALT
   ),
 
   [_NORMAN] = LAYOUT_arrow(
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_arrow(
     KC_GRV,  KC_1,    KC_2,     KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,    KC_9,     KC_0,    KC_CAPS,
     _______, KC_MEH,  XX_TAB,   W_SNIP,     KC_F5,      KC_F3,      KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  TM_SSH,  KC_PIPE,
-    MOUS,    _______, _______,  _______,    _______,    TOP,        BOTTOM,     KC_HOME,    KC_END,  _______,  _______, _______,
-    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
+    _______, _______, _______,  _______,    _______,    _______,    _______,    _______,    SK_KANA, _______,  _______, _______,
+    _______, _______, _______,                          _______,    JP_HENK,                _______, _______,  _______, _______
   ),
 
   [_RAISE] = LAYOUT_arrow(
@@ -56,11 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_MOVE] = LAYOUT_arrow(
-    M_EXPS,  A(KC_1), A(KC_2),  A(KC_3),    A(KC_4),    A(KC_5),    V_ALTB,     TM_PREV,    _______, TM_NEXT,  _______, _______,
-    _______, _______, _______,  _______,    _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  _______, _______,
-    _______, _______, _______,  _______,    _______,    _______,    _______,    SK_KANA,    _______, _______,  _______, _______,
-    _______, _______, _______,                          _______,    JP_HENK,                _______, _______,  _______, _______
+    M_EXPS,  _______, _______,  _______,    _______,    _______,    V_ALTB,     TM_PREV,    _______, TM_NEXT,  _______, _______,
+    MOUS,    A(KC_1), A(KC_2),  A(KC_3),    A(KC_4),    A(KC_5),    KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  A(KC_9), _______,
+    _______, _______, _______,  _______,    _______,    TOP,        BOTTOM,     KC_HOME,    KC_END,  _______,  _______, _______,
+    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
   ),
+
 
   [_MOUS] = LAYOUT_arrow(
     QK_BOOT, KC_F1,   KC_F2,    KC_F3,      KC_F4,      KC_F5,      _______,    KC_BTN1,    _______, KC_BTN2,  _______, KC_MPLY,
