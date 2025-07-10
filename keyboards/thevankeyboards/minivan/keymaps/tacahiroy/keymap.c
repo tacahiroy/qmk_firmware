fbc01671a031c8c1b6863bb43806a35cff6c7ba8
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-07-10 23:01:54 +0900

    MiniVan: Optimise the MOVE layer

--- keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_arrow(
     KC_GRV,  KC_1,    KC_2,     KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,    KC_9,     KC_0,    KC_CAPS,
-    _______, KC_MEH,  XX_TAB,   W_SNIP,     KC_F5,      KC_F3,      KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  TM_SSH,  KC_PIPE,
-    _______, _______, _______,  _______,    _______,    _______,    _______,    _______,    SK_KANA, _______,  _______, _______,
+    _______, KC_MEH,  XX_TAB,   W_SNIP,     KC_F5,      KC_F3,      KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  KC_COLN, KC_PIPE,
+    _______, _______, _______,  _______,    _______,    TOP,        BOTTOM,     KC_HOME,    KC_END,  _______,  _______, _______,
     _______, _______, _______,                          _______,    JP_HENK,                _______, _______,  _______, _______
   ),
 
@@ -57,16 +57,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOVE] = LAYOUT_arrow(
     M_EXPS,  _______, _______,  _______,    A(KC_9),    A(KC_D),    V_ALTB,     TM_PREV,    _______, TM_NEXT,  _______, _______,
-    MOUS,    A(KC_1), A(KC_2),  A(KC_3),    A(KC_4),    A(KC_5),    KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  _______, _______,
-
-    _______, _______, _______,  _______,    _______,    TOP,        BOTTOM,     KC_HOME,    KC_END,  _______,  _______, _______,
+    MOUS,    A(KC_1), A(KC_2),  A(KC_3),    A(KC_4),    A(KC_5),    KC_MS_L,    KC_MS_D,    KC_MS_U, KC_MS_R,  TM_SSH,  _______,
+    _______, _______, _______,  _______,    _______,    _______,    _______,    KC_BTN1,    KC_BTN2, _______,  _______, _______,
     _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
   ),
 
 
   [_MOUS] = LAYOUT_arrow(
-    QK_BOOT, KC_F1,   KC_F2,    KC_F3,      KC_F4,      KC_F5,      _______,    KC_BTN1,    _______, KC_BTN2,  _______, KC_MPLY,
-    _______, KC_F6,   KC_F7,    KC_F8,      KC_F9,      KC_F10,     KC_MS_L,    KC_MS_D,    KC_MS_U, KC_MS_R,  KC_VOLD, KC_VOLU,
+    QK_BOOT, KC_F1,   KC_F2,    KC_F3,      KC_F4,      KC_F5,      _______,    _______,    _______, _______,  _______, KC_MPLY,
+    _______, KC_F6,   KC_F7,    KC_F8,      KC_F9,      KC_F10,     _______,    _______,    _______, _______,  KC_VOLD, KC_VOLU,
     _______, _______, _______,  _______,    KC_F11,     KC_F12,     _______,    _______,    _______, _______,  _______, _______,
     _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
   ),
