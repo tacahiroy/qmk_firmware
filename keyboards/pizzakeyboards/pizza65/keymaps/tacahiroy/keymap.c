2f7d33f7033fdc9ba25e1cbbb2268ccb066d357b
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-08-12 15:09:59 +0900

    Pizza65: Add Caps related things

--- keyboards/pizzakeyboards/pizza65/keymaps/tacahiroy/keymap.c
+++ keyboards/pizzakeyboards/pizza65/keymaps/tacahiroy/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_65_ansi_blocker(
-        KC_GRV,  A(KC_1), A(KC_2), A(KC_3), A(KC_4), A(KC_5), _______, C_LEFT,  C_RIGHT, _______, _______, _______, _______, _______,   KC_MPLY,
+        KC_GRV,  _______, _______, _______, _______, _______, _______, C_LEFT,  C_RIGHT, _______, _______, _______, _______, _______,   KC_MPLY,
         W_WLST,  XX_WIN,  XX_TAB,  KC_MEH,  KC_CAPS, _______, SK_KANA ,TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, _______, CW_TOGG, _______,   KC_VOLU,
         _______, KC_MPLY, MOUS,    W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______,            KC_VOLD,
         _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______,          TOP,       _______,
