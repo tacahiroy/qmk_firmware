42e42a220ceee866569852a1db16db616890defa
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-09-16 15:50:54 +0900

    MiniVan: Optimise some key positions

--- keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/tacahiroy/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_arrow(
     KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
     CTL_X(ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT,    MO_X(Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LGUI,    KC_LALT, MOVE,                 LW_X(ENT),    RS_X(SPC),        KC_BSPC, RAISE,   KC_RCTL, KC_RALT
   ),
 
@@ -42,37 +42,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_arrow(
-    KC_GRV,  KC_1,    KC_2,     KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,    KC_9,     KC_0,    KC_CAPS,
-    _______, KC_MEH,  XX_TAB,   W_SNIP,     KC_F5,      KC_F3,      KC_LEFT,    KC_DOWN,    KC_UP,   KC_RGHT,  KC_COLN, _______,
-    _______, _______, _______,  _______,    _______,    TOP,        BOTTOM,     KC_HOME,    KC_END,  _______,  _______, _______,
-    _______, _______, _______,                          _______,    JP_HENK,                _______, _______,  _______, _______
+    KC_GRV,  KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,     KC_0,    KC_CAPS,
+    _______, KC_MEH,  XX_TAB,   W_SNIP,   KC_F5,    KC_F3,    KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  KC_COLN, _______,
+    _______, KC_VOLD, KC_VOLU,  KC_MPLY,  _______,  TOP,      BOTTOM,   KC_HOME,  KC_END,  _______,  _______, _______,
+    _______, _______, _______,                      _______,  JP_HENK,            _______, _______,  _______, _______
   ),
 
   [_RAISE] = LAYOUT_arrow(
-    KC_ESC,  KC_EXLM, KC_AT,    KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR, FX_NEXT,  _______, XX_WIN,
-    _______, _______, KC_EQL,   KC_PLUS,    KC_LPRN,    KC_LBRC,    KC_RBRC,    KC_RPRN,    KC_DEL,  KC_BSLS,  _______, _______,
-    _______, _______, CUT,      COPY2,      PASTE2,     KC_LCBR,    KC_RCBR,    KC_PGUP,    KC_PGDN, _______,  _______, _______,
-    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
+    KC_ESC,  KC_EXLM, KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  FX_NEXT,  _______,  XX_WIN,
+    _______, _______, KC_EQL,   KC_PLUS,  KC_LPRN,  KC_LBRC,  KC_RBRC,  KC_RPRN,  KC_DEL,   KC_BSLS,  _______,  _______,
+    _______, _______, CUT,      COPY2,    PASTE2,   KC_LCBR,  KC_RCBR,  KC_PGUP,  KC_PGDN,  _______,  _______,  _______,
+    _______, _______, _______,                      _______,  _______,            _______,  _______,  _______,  _______
   ),
 
   [_MOVE] = LAYOUT_arrow(
-    M_EXPS,  _______, _______,  _______,    A(KC_9),    A(KC_D),    V_ALTB,     TM_PREV,    _______, TM_NEXT,  _______, _______,
-    _______, A(KC_1), A(KC_2),  A(KC_3),    A(KC_4),    A(KC_5),    KC_MS_L,    KC_MS_D,    KC_MS_U, KC_MS_R,  TM_SSH,  KC_PIPE,
-    _______, _______, _______,  _______,    _______,    _______,    _______,    KC_BTN1,    KC_BTN2, _______,  _______, _______,
-    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
+    M_EXPS,  KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,   V_ALTB,   TM_PREV,  _______,  TM_NEXT,  _______,  _______,
+    _______, KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  MS_LEFT,  MS_DOWN,  MS_UP,    MS_RGHT,  TM_SSH,   KC_PIPE,
+    _______, _______, _______,  _______,  KC_F11,  KC_F12,  _______,  MS_BTN1,  MS_BTN2,  _______,  _______,  QK_BOOT,
+    _______, _______, _______,                     _______, _______,            _______,  _______,  _______,  _______
   ),
 
-  [_MOUS] = LAYOUT_arrow(
-    _______, KC_F1,   KC_F2,    KC_F3,      KC_F4,      KC_F5,      _______,    _______,    _______, _______,  _______, KC_MPLY,
-    _______, KC_F6,   KC_F7,    KC_F8,      KC_F9,      KC_F10,     _______,    _______,    _______, _______,  KC_VOLD, KC_VOLU,
-    _______, _______, _______,  _______,    KC_F11,     KC_F12,     _______,    _______,    _______, _______,  _______, QK_BOOT,
-    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
+  [_aOUS] = LAYOUT_arrow(
+    _______, _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______, _______,
+    _______, _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______, _______,
+    _______, _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______, _______,
+    _______, _______, _______,                      _______,  _______,            _______, _______,  _______, _______
   ),
 
   [_ADJUST] = LAYOUT_arrow(
-    QK_BOOT, QWERTY,  _______,  _______,    _______,    _______,    _______,    _______,    _______, KC_SYRQ,  KC_SCRL, KC_BRK,
-    _______, CADEL,   _______,  _______,    _______,    DB_TOGG,    _______,    NORMAN,     _______, _______,  _______, _______,
-    _______, _______, _______,  COLEMAK,    VERSION,    _______,    _______,    _______,    _______, _______,  _______, _______,
-    _______, _______, _______,                          _______,    _______,                _______, _______,  _______, _______
+    QK_BOOT, QWERTY,  _______,  _______,  _______,  _______,  _______,  _______,  _______, KC_SYRQ,  KC_SCRL, KC_BRK,
+    _______, CADEL,   _______,  _______,  _______,  DB_TOGG,  _______,  NORMAN,   _______, _______,  _______, _______,
+    _______, _______, _______,  COLEMAK,  VERSION,  _______,  _______,  _______,  _______, _______,  _______, _______,
+    _______, _______, _______,                      _______,  _______,            _______, _______,  _______, _______
   )
 };
--- users/tacahiroy/tacahiroy.c
+++ users/tacahiroy/tacahiroy.c
@@ -1,4 +1,4 @@
-/* Copyright 2020-2023 Takahiro Yoshihara <tacahiroy@gmail.com>
+/* Copyright 2020-2025 Takahiro YOSHIHARA <tacahiroy@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
