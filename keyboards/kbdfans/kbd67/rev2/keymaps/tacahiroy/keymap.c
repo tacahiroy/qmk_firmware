1f1fd96a482bcf80266c95bbb7a2d9feec6592a7
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2025-12-18 13:44:09 +0900

    EQL is used often

--- keyboards/kbdfans/kbd67/rev2/keymaps/tacahiroy/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/tacahiroy/keymap.c
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_65_ansi_blocker(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XX_WIN,    KC_F13,
-        AHK_ATB, AHK_MOB, XX_TAB,  _______, _______, KC_LBRC, KC_RBRC, _______, _______, FX_NEXT, _______, _______, KC_VOLD, KC_VOLU,   _______,
-        _______, KC_MPLY, _______, W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,            _______,
+        AHK_ATB, AHK_MOB, XX_TAB,  _______, _______, KC_LBRC, KC_RBRC, _______, KC_DEL,  FX_NEXT, _______, _______, KC_VOLD, KC_VOLU,   _______,
+        _______, KC_MPLY, KC_EQL,  W_SNIP,  KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,            _______,
         _______, AHK_ZOM, CUT,     COPY2,   PASTE2,  KC_LCBR, KC_RCBR, KC_HOME, KC_END, _______, _______, _______,          _______,   QK_BOOT,
         _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
     ),
