715def920e9d7e01bc008d34be54f024f054dfcb
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2026-02-26 14:16:41 +0900

    R-Control on LOWER is used often, so to avoid conflicts

--- keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
+++ keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_LOWER] = LAYOUT_default(
         W_IME,    KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_PIPE,  TM_WLST,  KC_TILD,
-		_______,  KC_CAPS,  SYSTEM,   XX_TAB,   W_SNIP,  KC_F5,    KC_F3,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   _______,  _______,
+		_______,  KC_CAPS,  SYSTEM,   XX_TAB,   W_SNIP,  KC_F5,    KC_F3,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   JP_MHEN,  _______,
         _______,  _______,  XXXXXXX,  _______,  CUT,     COPY2,    PASTE2,   TOP,      BOTTOM,   KC_HOME,  KC_END,   _______,  TM_SLST,  _______,  XXXXXXX,
-        _______,  _______,  _______,            _______,                     JP_HENK,                      JP_MHEN,            _______
+        _______,  _______,  _______,            _______,                     JP_HENK,                      _______,            _______
     ),
 
 	[_RAISE] = LAYOUT_default(
