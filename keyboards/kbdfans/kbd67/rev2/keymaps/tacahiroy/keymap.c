9a86043461989b89de453628a78ea1aff1aa8ddc
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2026-03-12 14:05:11 +0900

    KBD67 rev2: formatting

--- keyboards/kbdfans/kbd67/rev2/keymaps/tacahiroy/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/tacahiroy/keymap.c
@@ -20,40 +20,40 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_65_ansi_blocker(
-            QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_DEL,
-            KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,   KC_PGUP,
-            KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,    KC_PGDN,
-            SC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,          KC_UP,     MOUS,
-            KC_LGUI,    KC_LALT, LOWER,                     RS_X(SPC),                         KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN,   KC_RGHT
+        QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_DEL,
+        KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,   KC_PGUP,
+        KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,    KC_PGDN,
+        SC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,          KC_UP,     MOUS,
+        KC_LGUI,    KC_LALT, LOWER,                     RS_X(SPC),                         KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN,   KC_RGHT
     ),
 
     [_NORMAN] = LAYOUT_65_ansi_blocker(
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,
-            _______, KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, _______, _______, _______,   _______,
-            _______, KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    _______, _______,            _______,
-            _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    _______, _______, _______, _______,          _______,   _______,
-            _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,
+        _______, KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, _______, _______, _______,   _______,
+        _______, KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    _______, _______,            _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    _______, _______, _______, _______,          _______,   _______,
+        _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
     ),
 
     [_QWERTY] = LAYOUT_65_ansi_blocker(
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,
-            _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______, _______, _______,   _______,
-            _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______, _______,            _______,
-            _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    _______, _______, _______, _______,          _______,   _______,
-            _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,
+        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______, _______, _______,   _______,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______, _______,            _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    _______, _______, _______, _______,          _______,   _______,
+        _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
     ),
 
     [_LOWER] = LAYOUT_65_ansi_blocker(
-            KC_GRV,  _______, _______, _______, _______, _______, _______, C_LEFT,  C_RIGHT, _______, _______, _______, _______, _______,   KC_MPLY,
-            W_WLST,  _______, XX_TAB,  KC_MEH,  KC_CAPS, _______, JP_HENK, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, _______, _______, _______,   KC_VOLU,
-            _______, KC_MPLY, MOUS,    W_SNIP,  _______, _______, _______, KC_PGDN, KC_PGUP, KC_BSLS, TM_SSH,  JP_MHEN, _______,            KC_VOLD,
-            _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______,          TOP,       _______,
-            _______, _______, _______,                   SK_KANA,                            _______, _______,          _______, BOTTOM,    _______
+        KC_GRV,  _______, _______, _______, _______, _______, _______, C_LEFT,  C_RIGHT, _______, _______, _______, _______, _______,   KC_MPLY,
+        W_WLST,  _______, XX_TAB,  KC_MEH,  KC_CAPS, _______, JP_HENK, TM_PREV, TM_WLST, TM_NEXT, KC_PIPE, _______, _______, _______,   KC_VOLU,
+        _______, KC_MPLY, MOUS,    W_SNIP,  _______, _______, _______, KC_PGDN, KC_PGUP, KC_BSLS, TM_SSH,  JP_MHEN, _______,            KC_VOLD,
+        _______, _______, CUT,     COPY2,   PASTE2,  TOP,     BOTTOM,  KC_HOME, KC_END,  _______, _______, _______,          TOP,       _______,
+        _______, _______, _______,                   SK_KANA,                            _______, _______,          _______, BOTTOM,    _______
     ),
 
     [_RAISE] = LAYOUT_65_ansi_blocker(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XX_WIN,    KC_F13,
-        AHK_ATB, AHK_MOB, XX_TAB,  _______, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_DEL,  FX_NEXT, _______, KC_VOLD, KC_VOLU, KC_MPLY,   _______,
+        AHK_ATB, AHK_MOB, XX_TAB,  _______, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_DEL,  FX_NEXT, _______, _______, _______, _______,   _______,
         _______, _______, KC_EQL,  KC_PLUS, KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,            _______,
         _______, AHK_ZOM, CUT,     COPY2,   PASTE2,  KC_LCBR, KC_RCBR, KC_HOME, KC_END,  _______, _______, _______,          _______,   QK_BOOT,
         _______, _______, _______,                   _______,                            _______, _______,          _______, _______,   _______
