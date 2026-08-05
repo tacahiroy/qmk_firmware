952b4ce7d3804e052ab047fdea32ab4d89c7e876
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2026-08-05 15:56:51 +0900

    TMOv2: Adjust position: vertical-bar

--- keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
+++ keyboards/cannonkeys/tmov2/keymaps/tacahiroy/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[_LOWER] = LAYOUT_default(
-        W_IME,    KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  KC_CAPS,  KC_TILD,
-		_______,  _______,  SYSTEM,   XX_TAB,   W_SNIP,  KC_F5,    KC_F3,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   JP_MHEN,  _______,
+        W_IME,    KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  KC_PIPE,  KC_TILD,
+		_______,  _______,  KC_CAPS,  XX_TAB,   W_SNIP,  KC_F5,    KC_F3,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  TM_SSH,   JP_MHEN,  _______,
         _______,  _______,  XXXXXXX,  _______,  CUT,     COPY2,    PASTE2,   TOP,      BOTTOM,   KC_HOME,  KC_END,   _______,  _______,  _______,  XXXXXXX,
         _______,  _______,  _______,            _______,                     SK_KANA,                      _______,            _______
     ),
