4b9b51349668005d8c3b1cf98e21a18adb177f3b
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2026-09-02 18:54:31 +0900

    Adjust positions for left and right Alt keys

--- keyboards/mechlovin/zed60/keymaps/tacahiroy/keymap.c
+++ keyboards/mechlovin/zed60/keymaps/tacahiroy/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,   KC_F,   KC_P,   KC_G,      KC_J,   KC_L,   KC_U,     KC_Y,    KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSPC,
     KC_LCTL,  KC_A,     KC_R,   KC_S,   KC_T,   KC_D,      KC_H,   KC_N,   KC_E,     KC_I,    KC_O,     KC_QUOT,  KC_ENT,
     SC_LSPO,  KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,      KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  SC_RSPC,  KC_PGDN,
-    KC_LGUI,  KC_LALT,  LOWER,                  RS_X(SPC),                                    KC_RCTL,  KC_RALT,  KC_RGUI
+    KC_LGUI,  KC_LALT,  LOWER,                  RS_X(SPC),                                    KC_RCTL,  KC_RGUI,  KC_RALT
   ),
 
   [_NORMAN] = LAYOUT_60_tsangan_hhkb(
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LGUI,
-    KC_LALT,  XXXXXXX,  _______,                      _______,                                          _______,  XXXXXXX,  KC_RALT
+    KC_LALT,  KC_LGUI,  LOWER,                        _______,                                          KC_RCTL,  KC_RGUI,  KC_RALT
   ),
 
   [_QWERTY] = LAYOUT_60_tsangan_hhkb(
@@ -59,6 +59,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______, _______,
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    _______, _______, _______, _______, _______,
     _______, _______, _______,                   _______,                                     _______, _______, _______
+  ),
+
+  [_TEN_U] = LAYOUT_60_tsangan_hhkb(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LGUI,
+    KC_LALT,  LOWER,    XXXXXXX,                      _______,                                          XXXXXXX,  KC_RCTL,  KC_RALT
   ),
 
   [_LOWER] = LAYOUT_60_tsangan_hhkb(
@@ -75,14 +83,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_MPLY, KC_EQL,  KC_PLUS, KC_F5,   KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TM_SSH,  _______, _______,
     _______, AHK_ZOM, TM_COPY, COPY2,   PASTE2,  KC_LCBR, KC_RCBR, KC_HOME, KC_END,  _______, _______, _______, QK_BOOT,
     _______, _______, _______,                   _______,                            _______, _______, _______
-  ),
-
-  [_TEN_U] = LAYOUT_60_tsangan_hhkb(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LGUI,
-    KC_LALT,  LOWER,    XXXXXXX,                      _______,                                          XXXXXXX,  KC_RCTL,  KC_RALT
   ),
 
   [_MOUS] = LAYOUT_60_tsangan_hhkb(
